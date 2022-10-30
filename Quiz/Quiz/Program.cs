2441bf5698b39da4bdbcef6bf81c063aef6e4a8c
andrzej-herman <aherman@san.edu.pl>
2022-10-30 12:57:44 +0100

    30.10.2022

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine();
+
+// nastepne zajęcia zaczynamy od analizy obiektowej naszego Quizu
+// musimy ustalić jakie obiekty będą występowały w naszej aplikacji
+// a nastepnie przenieść do naszego programu
