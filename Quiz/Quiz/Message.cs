b3449d2895b6db8929b9de32aac8239893816acd
andrzej-herman <aherman@san.edu.pl>
2022-11-13 16:41:23 +0100

    13.11.2022

---
+++ Quiz/Quiz/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    public class Message
+    {
+        public void DisplayWelcomeScreen()
+        {
+            Console.WriteLine();
+            Console.WriteLine("WITAJ W QUZIE WIEDZY GRUPY 6");
+            Console.WriteLine("Spróbuj odpowiedzieć n a7 pytań");
+            Console.WriteLine("POWODZENIA !!!!");
+            Console.WriteLine();
+        }
+    }
+}
--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -1,5 +1,45 @@
-﻿Console.WriteLine();
+﻿using Quiz;
+using static System.Net.Mime.MediaTypeNames;
 
-// nastepne zajęcia zaczynamy od analizy obiektowej naszego Quizu
-// musimy ustalić jakie obiekty będą występowały w naszej aplikacji
-// a nastepnie przenieść do naszego programu
+var message = new Message();
+message.DisplayWelcomeScreen();
+
+
+
+
+//var q1 = new Question();
+//q1.Id = 2;
+//q1.Category = 100;
+//q1.Content = "W którym roku wybuchła II wojna?";
+
+//q1.Answer1 = new Answer();
+//q1.Answer1.Id = 1;
+//q1.Answer1.Content = "1939";
+//q1.Answer1.IsCorrect= true;
+
+//q1.Answer2 = new Answer();
+//q1.Answer2.Id = 2;
+//q1.Answer2.Content = "2000";
+//q1.Answer2.IsCorrect = false;
+
+//q1.Answer3 = new Answer();
+//q1.Answer3.Id = 3;
+//q1.Answer3.Content = "1945";
+//q1.Answer3.IsCorrect = false;
+
+//q1.Answer4 = new Answer();
+//q1.Answer4.Id = 4;
+//q1.Answer4.Content = "2012";
+//q1.Answer4.IsCorrect = false;
+
+
+
+
+
+
+
+
+Console.WriteLine();
+
+
+
